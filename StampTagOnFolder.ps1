386480db749df60dcaaa33c1c1fec491d55d7194
Akash Bhargava <akash_bhargava@hotmail.com>
2023-09-07 10:11:08 -0500

    Removing the Warning variable

--- StampTagOnFolder.ps1
+++ StampTagOnFolder.ps1
@@ -1,20 +1,8 @@
-#StampPTonTasks.ps1
-#
-#DISCLAIMER:
-# THIS CODE IS SAMPLE CODE. THESE SAMPLES ARE PROVIDED "AS IS" WITHOUT WARRANTY OF ANY KIND.
-# MICROSOFT FURTHER DISCLAIMS ALL IMPLIED WARRANTIES INCLUDING WITHOUT LIMITATION ANY IMPLIED WARRANTIES OF MERCHANTABILITY OR OF FITNESS FOR
-# A PARTICULAR PURPOSE. THE ENTIRE RISK ARISING OUT OF THE USE OR PERFORMANCE OF THE SAMPLES REMAINS WITH YOU. IN NO EVENT SHALL
-# MICROSOFT OR ITS SUPPLIERS BE LIABLE FOR ANY DAMAGES WHATSOEVER (INCLUDING, WITHOUT LIMITATION, DAMAGES FOR LOSS OF BUSINESS PROFITS,
-# BUSINESS INTERRUPTION, LOSS OF BUSINESS INFORMATION, OR OTHER PECUNIARY LOSS) ARISING OUT OF THE USE OF OR INABILITY TO USE THE
-# SAMPLES, EVEN IF MICROSOFT HAS BEEN ADVISED OF THE POSSIBILITY OF SUCH DAMAGES. BECAUSE SOME STATES DO NOT ALLOW THE EXCLUSION OR LIMITATION
-# OF LIABILITY FOR CONSEQUENTIAL OR INCIDENTAL DAMAGES, THE ABOVE LIMITATION MAY NOT APPLY TO YOU.
-
-#.\StampPTonTasks.ps1 -Username brians -Password (ConvertTo-SecureString password -AsPlainText -Force) -Domain shekhchilli -EwsUrl "https://mail.domain.us/ews/exchange.asmx" -IgnoreSSLCertificate
-#.\StampPTonTasks.ps1 -Username ServiceAccount -Password (ConvertTo-SecureString password -AsPlainText -Force) -Domain shekhchilli -EwsUrl "https://mail.domain.us/ews/exchange.asmx" -PersonalTag "{c6c9ad12-9e46-4b29-a053-e1ea189ab0cc}" -Impersonate -IgnoreSSLCertificate
-#.\StampPTonTasks.ps1 -Username UserOne@shekhchilli.us -Password (ConvertTo-SecureString password -AsPlainText -Force) -EwsUrl "https://outlook.office365.com/EWS/Exchange.asmx" -PersonalTag "{9adba0aa-ef3f-4cb4-a7f1-b5d5782c0e4e}"
-	
-#Example: $cred =Get-Credential
-#StampPTonTasks.ps1 -Credentials $cred -EwsUrl "https://mail.domain.com/ews/exchange.asmx" -IgnoreSSLCertificate
+# The script requires the EWS managed API, which can be downloaded here:
+# https://www.microsoft.com/downloads/details.aspx?displaylang=en&FamilyID=c3342fb3-fbcc-4127-becf-872c746840e1
+# This also requires PowerShell 2.0
+# Make sure the Import-Module command below matches the DLL location of the API.
+# This path must match the install location of the EWS managed API. Change it if needed.
 
 param (
 	[Parameter(Mandatory=$False,HelpMessage="Username used to authenticate with EWS")]
@@ -25,9 +13,6 @@ param (
 	
 	[Parameter(Mandatory=$False,HelpMessage="Domain used to authenticate with EWS")]
 	[string]$Domain,
-	
-	[Parameter(Mandatory=$False,HelpMessage="Credentials")]
-	[PSCredential]$Credentials,
 	
 	[Parameter(Mandatory=$False,HelpMessage="Whether we are using impersonation to access the mailbox")]
 	[switch]$Impersonate,
@@ -42,24 +27,16 @@ param (
 	[switch]$IgnoreSSLCertificate,
 	
 	[Parameter(Mandatory=$False,HelpMessage="Whether to allow insecure redirects when performing autodiscover")]	
-	[switch]$AllowInsecureRedirection,
-	
-	[Parameter(Mandatory=$False,HelpMessage="Log file - activity is logged to this file if specified")]	
-	[string]$LogFile = "",
-
-	[Parameter(Mandatory=$False,HelpMessage="Personal Tag to Stamp - Use the RetentionId from the Retention Policy")]
-	[string]$PersonalTag
-	
-)
+	[switch]$AllowInsecureRedirection
+	)
 
 [string]$info = "White"                # Color for informational messages
+[string]$warning = "Yellow"            # Color for warning messages
 [string]$error = "Red"                 # Color for error messages
-[string]$success = "Green"             # Color for success messages
-[string]$LogFile = "StampPTonTasks.txt"   # Path of the Log File
+[string]$LogFile = "StampTagOnFolderLog.txt"   # Path of the Log File
 
-$verbose =$true
+$verbose = $False
 
-# Define our functions
 Function Log([string]$Details, [ConsoleColor]$Colour)
 {
     if ($Colour -eq $null)
@@ -77,6 +54,7 @@ Function Log([string]$Details, [ConsoleColor]$Colour)
 Function LoadEWSManagedAPI()
 {
 	# Find and load the managed API
+	
 	if ( ![string]::IsNullOrEmpty($EWSManagedApiPath) )
 	{
 		if ( Test-Path $EWSManagedApiPath )
@@ -112,6 +90,7 @@ Function TrustAllCerts() {
     ## Code From http://poshcode.org/624
     ## Create a compilation environment
     $Provider=New-Object Microsoft.CSharp.CSharpCodeProvider
+    #$Compiler=$Provider.CreateCompiler()
     $Params=New-Object System.CodeDom.Compiler.CompilerParameters
     $Params.GenerateExecutable=$False
     $Params.GenerateInMemory=$True
@@ -144,7 +123,7 @@ Function TrustAllCerts() {
 function CreateService($targetMailbox)
 {
     # Creates and returns an ExchangeService object to be used to access mailboxes
-    $exchangeService = New-Object Microsoft.Exchange.WebServices.Data.ExchangeService([Microsoft.Exchange.WebServices.Data.ExchangeVersion]::Exchange2013_SP1)
+    $exchangeService = New-Object Microsoft.Exchange.WebServices.Data.ExchangeService([Microsoft.Exchange.WebServices.Data.ExchangeVersion]::Exchange2010_SP1)
 
     # Set credentials if specified, or use logged on user.
     if ($Null -ne $Credentials)
@@ -206,51 +185,55 @@ function CreateService($targetMailbox)
     return $exchangeService
 }
 
-function StampPolicyOnFolder ($Mailbox)
+function StampPolicyOnFolder($MailboxName)
 {
-    # Process the mailbox 
-	Log([string]::Format("Processing mailbox {0}", $Mailbox)) $info
+    Log "Stamping Policy on folder for Mailbox Name: '$($MailboxName)'" $info
 
-    $global:service=CreateService($Mailbox)
+    #Change the user to Impersonate
+    $service.ImpersonatedUserId = new-object Microsoft.Exchange.WebServices.Data.ImpersonatedUserId([Microsoft.Exchange.WebServices.Data.ConnectingIdType]::SmtpAddress,$MailboxName);    #Search for the folder you want to stamp the property on
+    $oFolderView = new-object Microsoft.Exchange.WebServices.Data.FolderView(1)
+    $oSearchFilter = new-object Microsoft.Exchange.WebServices.Data.SearchFilter+IsEqualTo([Microsoft.Exchange.WebServices.Data.FolderSchema]::DisplayName,$FolderName)    #Uncomment the line below if the folder is in the regular mailbox
+    $oFindFolderResults = $service.FindFolders([Microsoft.Exchange.WebServices.Data.WellKnownFolderName]::MsgFolderRoot,$oSearchFilter,$oFolderView)    
     
-    if($Null -eq $global:service)
+    if ($oFindFolderResults.TotalCount -eq 0)
     {
-        Write-Host"Failed to create ExchangeService" -ForegroundColor Red
-    }
-
-    Log([string]::Format("Stamping Policy on Tasks folder for Mailbox Name:{0}", $Mailbox)) $info
-		
-	$oFolder =  [Microsoft.Exchange.WebServices.Data.Folder]::Bind($service,[Microsoft.Exchange.WebServices.Data.WellKnownFolderName]::Tasks)
-
-    if ($null -eq $oFolder)
-    {
-		Log "Folder does not exist in Mailbox Name: $($Mailbox)." $error
+        Log "Folder does not exist in Mailbox: '$($MailboxName)'" $warning
     }
     else
     {
-		try {
+        Log "Folder found in Mailbox: '$($MailboxName)'" $info
 
-			$ArchiveTagGUID = new-Object Guid($PersonalTag);
-			$oFolder.ArchiveTag = new-object Microsoft.Exchange.WebServices.Data.ArchiveTag($true, $ArchiveTagGUID.ToByteArray())
-			$oFolder.Update()
+        #PR_POLICY_TAG 0x3019
+        $PolicyTag = New-Object Microsoft.Exchange.WebServices.Data.ExtendedPropertyDefinition(0x3019,[Microsoft.Exchange.WebServices.Data.MapiPropertyType]::Binary);        
+        
+        #PR_RETENTION_FLAGS 0x301D    
+        $RetentionFlags = New-Object Microsoft.Exchange.WebServices.Data.ExtendedPropertyDefinition(0x301D,[Microsoft.Exchange.WebServices.Data.MapiPropertyType]::Integer);
+        
+        #PR_RETENTION_PERIOD 0x301A
+        $RetentionPeriod = New-Object Microsoft.Exchange.WebServices.Data.ExtendedPropertyDefinition(0x301A,[Microsoft.Exchange.WebServices.Data.MapiPropertyType]::Integer);        
+        
+        #Bind to the folder found
+        $oFolder = [Microsoft.Exchange.WebServices.Data.Folder]::Bind($service,$oFindFolderResults.Folders[0].Id)
+       
+        #Same as the value in the PR_RETENTION_FLAGS property
+        $oFolder.SetExtendedProperty($RetentionFlags, 137)        #Same as the value in the PR_RETENTION_PERIOD property
+        $oFolder.SetExtendedProperty($RetentionPeriod, 1095)        #Change the GUID based on your policy tag
+        $PolicyTagGUID = new-Object Guid("{92186ff7-7f4d-4efa-a09b-bbdc5aee3908}");
 
-			Log "Retention policy stamped!`n`r" $success
-			
-		}
-		catch {
-			Log([string]::Format("Error:{0}`n`r", $_.Exception.Message)) $error
-		}
-  	}
-    
-  $global:service=$Null
+        $oFolder.SetExtendedProperty($PolicyTag, $PolicyTagGUID.ToByteArray())  
+
+        $oFolder.Update()
+
+        Log "Retention policy stamped!" $info
+    }    
+
+    $service.ImpersonatedUserId = $null
 }
 
-# The following is the start of the main script
-if (!$PersonalTag)
-{
-	Write-Host "No Personal Tag Specified!" -ForegroundColor Red
-	Exit
-}
+#Change the name of the folder. This is the folder the properties will be stamped on.
+$FolderName = "My Folder"
+
+# The following is the main script
 
 # Check if we need to ignore any certificate errors
 # This needs to be done *before* the managed API is loaded, otherwise it doesn't work consistently (i.e. usually doesn't!)
@@ -268,7 +251,7 @@ if (!(LoadEWSManagedAPI))
 }
 
 # Check we have valid credentials
-if ($Credentials -ne $Null)
+if ($Null -ne $Credentials)
 {
     If ($Username -or $Password)
     {
@@ -298,13 +281,12 @@ If ( $FileExists )
         {
             if (!$entry.PrimarySmtpAddress.ToLower().Equals("primarysmtpaddress"))
             {
-		        $Mailbox = $entry.PrimarySmtpAddress
-			    StampPolicyOnFolder $Mailbox
+			    StampPolicyOnFolder($entry.PrimarySmtpAddress)
             }
         }
 	}
 }
-Else
+else
 {
 	Write-Host "Did not find the CSV file to read the mailboxes from."
 }
